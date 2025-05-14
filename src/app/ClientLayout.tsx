8f3eafb196ddc62faa16f5fb31c0401349d9c3a0
saessak <gkstotkr1201@gmail.com>
2025-05-14 16:34:50 +0900

    Revert "[Modify] 삼성 구버전 웹브라우저일 경우 alert 제공"
    This reverts commit 9cb25a74ee5508c950842c29db6fed636edaa2c1.

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -7,7 +7,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AlertModal from "@/components/AlertModal";
 import "@/lib/i18n";
 import i18n from "@/lib/i18n";
-import BrowserUpdateAlert from "@/components/BrowserUpdateAlert";
 
 export const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
@@ -61,18 +60,41 @@ export const ClientLayout = ({ children }: { children: React.ReactNode }) => {
     }
   }, [router]);
 
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    if (currentPath !== "/" && sessionStorage.length > 1) {
+      router.push(currentPath);
+    } else if (
+      sessionStorage.length < 1 &&
+      currentPath !== "/" &&
+      currentPath !== "/login" &&
+      currentPath !== "/repassword"
+    ) {
+      router.push("/login");
+    }
+
+    if (i18n.isInitialized) {
+      setI18nReady(true);
+    } else {
+      i18n.on('initialized', () => {
+        setI18nReady(true);
+      });
+    }
+  }, [router]);
+
   if (!i18nReady) {
     // 초기화 전에는 아무 것도 안 보여줌
     return null;
+    // 또는 로딩 컴포넌트
+    // return <div>Loading...</div>;
   }
 
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <div className={`layout ${orientationClass}`}>
-          <BrowserUpdateAlert />
           {children}
-          <AlertModal />
+        <AlertModal />
         </div>
       </QueryClientProvider>
     </RecoilRoot>
