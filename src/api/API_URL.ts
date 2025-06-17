b794d52dfded90d46cd1c84ee38212a141d8b618
saessak <gkstotkr1201@gmail.com>
2025-06-17 16:04:07 +0900

    [Modify] api url 수정

--- src/api/API_URL.ts
+++ src/api/API_URL.ts
@@ -1,2 +1,9 @@
-export const API_URL = 'http://43.202.78.220:7110/mng/v1/'
-export const GPS_URL = 'http://43.202.78.220:7120/mng/v1/'
+const originURL = (subdomain: string, path = "/mng/v1/") => {
+  const origin = window.location.origin.includes("localhost") ? "http://43.202.78.220:7010" : window.location.origin;
+  return (window.location.protocol.startsWith("https")
+    ? window.location.origin.replace('mo', "gps" === subdomain ? "gpis" : "apis")
+    : origin.replace(':7010', ('gps' === subdomain) ? ":7120" : ":7110")) + path;
+};
+
+export const API_URL =  originURL("api");
+export const GPS_URL = originURL("gps");
