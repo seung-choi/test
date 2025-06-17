1165cbb2a282d894f6b083897740e8aa7e53894b
saessak <gkstotkr1201@gmail.com>
2025-06-17 16:09:02 +0900

    [Modify] api url 수정(window 객체 사용못함)

--- src/api/API_URL.ts
+++ src/api/API_URL.ts
@@ -1,9 +1,17 @@
-const originURL = (subdomain: string, path = "/mng/v1/") => {
-  const origin = window.location.origin.includes("localhost") ? "http://43.202.78.220:7010" : window.location.origin;
+export const getOriginURL = (subdomain: string, path = "/mng/v1/"): string | null => {
+  if (typeof window === "undefined") {
+    // SSR 환경에선 window 없음
+    return null;
+  }
+
+  const origin = window.location.origin.includes("localhost")
+    ? "http://43.202.78.220:7010"
+    : window.location.origin;
+
   return (window.location.protocol.startsWith("https")
-    ? window.location.origin.replace('mo', "gps" === subdomain ? "gpis" : "apis")
-    : origin.replace(':7010', ('gps' === subdomain) ? ":7120" : ":7110")) + path;
+    ? origin.replace("mo", subdomain === "gps" ? "gpis" : "apis")
+    : origin.replace(":7010", subdomain === "gps" ? ":7120" : ":7110")) + path;
 };
 
-export const API_URL =  originURL("api");
-export const GPS_URL = originURL("gps");
+export const API_URL =  getOriginURL("api");
+export const GPS_URL = getOriginURL("gps");
