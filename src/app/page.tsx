233017c2f493bd5dd74ec35d147f8a7e8ded9abd
hss <hss@pluxity.com>
2025-10-02 15:21:52 +0900

    [fix] 스플래시 페이지에서 accessToken값 유무에 따라 조건 라우터 처리.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,20 @@
 import styles from "@/styles/pages/index.module.scss";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import storage from "@/utils/storage";
 
 export default function Splash() {
   const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push("/login/"); // 로그인 화면으로 이동
+      // accessToken이 있으면 monitoring 페이지로, 없으면 login 페이지로 이동
+      const accessToken = storage.local.get("accessToken");
+      if (accessToken) {
+        router.push("/monitoring");
+      } else {
+        router.push("/login");
+      }
     }, 2000); // 2초 후에 실행
 
     return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
