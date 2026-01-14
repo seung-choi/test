4cfdd3a10086786afed9f980e2a381d5ffec7cfe
seungeun <seungeun@pluxity.com>
2026-01-14 14:46:53 +0900

    [refactor] 초기 페이지 로그인으로 수정, 스플래시 이미지 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,5 @@
-"use client";
-
-import styles from "@/styles/pages/index.module.scss";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import storage from "@/utils/storage";
+import { redirect } from "next/navigation";
 
 export default function Splash() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      // accessToken이 있으면 monitoring 페이지로, 없으면 login 페이지로 이동
-      const accessToken = storage.local.get("accessToken");
-      if (accessToken) {
-        router.push("/admin/lounge");
-      } else {
-        // router.push("/login");
-        router.push("/admin/lounge");
-      }
-    }, 2000); // 2초 후에 실행
-
-    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
-  }, [router]);
-
-  return (
-    <>
-      <div className={styles["splash-container"]}>
-        <img
-          style={{ width: "100%", height: "100%" }}
-          src={"/assets/image/img-vgolf-splash.png"}
-          alt={"스플래시 배경 이미지"}
-        />
-      </div>
-    </>
-  );
+  redirect("/login");
 }
